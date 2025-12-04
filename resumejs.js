371c3422f6cb60af3a5fa5e74f85c507ebd5de10
kkaleena <kaleena1226@gmail.com>
2025-12-04 11:35:08 -0500

    Delete resumejs.js

--- resumejs.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('downloadButton').addEventListener('click', function() {
-
-    document.getElementById('downloadLink1').click();
-    document.getElementById('downloadLink2').click();
-}
-);
