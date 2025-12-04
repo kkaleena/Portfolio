485cf9194e8f25f3144fe1a81c805ac86fd3bb38
kkaleena <kaleena1226@gmail.com>
2025-12-04 11:38:33 -0500

    Add files via upload

---
+++ js/resumejs.js
@@ -0,0 +1,6 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+
+    document.getElementById('downloadLink1').click();
+    document.getElementById('downloadLink2').click();
+}
+);
